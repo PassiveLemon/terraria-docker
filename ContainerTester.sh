c5a3bb2c1f9c9f9cd7d85349641e08d0c57a7903
PassiveLemon <lemonl3mn@protonmail.com>
2023-12-20 22:42:02 -0500

    Remove: ContainerTester.sh lol
    You dont need to see this script

--- ContainerTester.sh
+++
@@ -1,4 +0,0 @@
-docker kill a-terrariatester
-docker rm a-terrariatester
-
-docker run -d --name a-terrariatester -p 7777:7777/tcp -v /home/docker/Containers/Test/terraria-dockertest:/opt/terraria/config passivelemon/terraria-docker:1.4.4.9
