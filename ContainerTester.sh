07a7e915b123fdb9aca56c4d3b79a1e59ddf0666
PassiveLemon <lemonl3mn@protonmail.com>
2023-12-20 22:38:56 -0500

    Test?

--- ContainerTester.sh
+++ ContainerTester.sh
@@ -1,4 +1,4 @@
 docker kill a-terrariatester
 docker rm a-terrariatester
 
-docker run -d --name a-terrariatester -p 7777:7777/tcp -v /home/docker/Containers/Test/terraria-dockertest:/opt/terraria/config passivelemon/terraria-docker:terraria-tshock
+docker run -d --name a-terrariatester -p 7777:7777/tcp -v /home/docker/Containers/Test/terraria-dockertest:/opt/terraria/config passivelemon/terraria-docker:1.4.4.9
