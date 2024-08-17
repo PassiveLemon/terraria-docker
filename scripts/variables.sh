ee349aba30f7dc42a4f9d73027e37579634e6e0c
PassiveLemon <lemonl3mn@protonmail.com>
2024-08-17 14:16:08 -0400

    Refactor(scripts): Use an array for variables. Fixes #2

--- scripts/variables.sh
+++ scripts/variables.sh
@@ -1,37 +1,39 @@
 #!/usr/bin/env bash
 
+declare -A VAR_ARRAY
+
 # Define server variables for config pasting later. Very crude and ugly
-AUTOCREATEx="autocreate=${AUTOCREATE}"
-DIFFICULTYx="difficulty=${DIFFICULTY}"
-BANLISTx="banlist=${BANLIST}"
-LANGUAGEx="language=${LANGUAGE}"
-MAXPLAYERSx="maxplayers=${MAXPLAYERS}"
+VAR_ARRAY[AUTOCREATEx]="autocreate=${AUTOCREATE}"
+VAR_ARRAY[DIFFICULTYx]="difficulty=${DIFFICULTY}"
+VAR_ARRAY[BANLISTx]="banlist=${BANLIST}"
+VAR_ARRAY[LANGUAGEx]="language=${LANGUAGE}"
+VAR_ARRAY[MAXPLAYERSx]="maxplayers=${MAXPLAYERS}"
 if [ "$MOTD" != "" ]; then
-  MOTDx="motd=${MOTD}"
+  VAR_ARRAY[MOTDx]="motd=${MOTD}"
 fi
-NPCSTREAMx="npcstream=${NPCSTREAM}"
+VAR_ARRAY[NPCSTREAMx]="npcstream=${NPCSTREAM}"
 if [ "$PASSWORD" != "" ]; then
-  PASSWORDx="password=${PASSWORD}"
+  VAR_ARRAY[PASSWORDx]="password=${PASSWORD}"
 fi
-PORTx="port=7777"
-PRIORITYx="priority=${PRIORITY}"
-SECUREx="secure=${SECURE}"
+VAR_ARRAY[PORTx]="port=7777"
+VAR_ARRAY[PRIORITYx]="priority=${PRIORITY}"
+VAR_ARRAY[SECUREx]="secure=${SECURE}"
 if [ "$SEED" != "" ]; then
-  SEEDx="seed=${SEED}"
+  VAR_ARRAY[SEEDx]="seed=${SEED}"
 fi
-UPNPx="upnp=${UPNP}"
-WORLDNAMEx="worldname=${WORLDNAME}"
+VAR_ARRAY[UPNPx]="upnp=${UPNP}"
+VAR_ARRAY[WORLDNAMEx]="worldname=${WORLDNAME}"
 
 # Automatically set variables
-WORLDx="world=/opt/terraria/config/Worlds/${WORLDNAME}.wld"
+VAR_ARRAY[WORLDx]="world=/opt/terraria/config/Worlds/${WORLDNAME}.wld"
 
 # Write variables to file
-cd /opt/terraria/config/
 if [ "$SERVERCONFIG" = "0" ]; then
   if [ -e "/opt/terraria/config/serverconfig.txt" ]; then
     rm /opt/terraria/config/serverconfig.txt
   fi
-  for argument in $AUTOCREATEx $DIFFICULTYx $BANLISTx $LANGUAGEx $MAXPLAYERSx $MOTDx $NPCSTREAMx $PASSWORDx $PORTx $PRIORITYx $SEEDx $SECUREx $UPNPx $WORLDNAMEx $WORLDx; do
-    echo $argument >> serverconfig.txt
+  for argument in "${VAR_ARRAY[@]}"; do
+    echo "$argument" >> /opt/terraria/config/serverconfig.txt
   done
 fi
+
