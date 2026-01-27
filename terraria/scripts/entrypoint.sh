b56498bb089a0f7d7b0530df8815c45873fedf3b
sloth-on-meth <info.sbphotographics@gmail.com>
2026-01-27 23:54:47 +0100

    up

--- terraria/scripts/entrypoint.sh
+++ terraria/scripts/entrypoint.sh
@@ -2,6 +2,16 @@
 
 set -e
 
+PUID="${PUID:-1000}"
+PGID="${PGID:-1000}"
+
+case "$PUID" in
+  ''|*[!0-9]*) echo "Invalid PUID: $PUID" >&2; exit 2 ;;
+esac
+case "$PGID" in
+  ''|*[!0-9]*) echo "Invalid PGID: $PGID" >&2; exit 2 ;;
+esac
+
 # Create terraria user for rootless operation
 if ! getent group terraria > /dev/null 2>&1; then
   groupadd -g "$PGID" terraria
