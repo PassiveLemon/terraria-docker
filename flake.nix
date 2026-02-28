2332870f21af3f9ef8505be4e27a0fa8890a4cdb
PassiveLemon <lemonl3mn@protonmail.com>
2026-02-28 02:01:20 -0500

    feature: nix flake dev

--- flake.nix
+++ flake.nix
@@ -15,6 +15,7 @@
     perSystem = { self', system, ... }:
     let
       pkgs = import inputs.nixpkgs { inherit system; };
+      lib = pkgs.lib;
     in
     {
       devShells = {
@@ -22,10 +23,49 @@
           packages = with pkgs; [
             act dive trivy
           ];
+          shellHook = ''
+            alias nr="nix run .#"
+          '';
         };
       };
-      packages = {
-        default = pkgs.act;
+      packages = let
+        build = lib.getExe self'.packages.build;
+        tVersion = "1.4.5.5";
+        tmlVersion = "2025.12.3.1";
+        # lib.replaceString doesn't exist?
+        tVersionTrim = lib.replaceStrings [ "." ] [ "" ] tVersion;
+      in {
+        default = self'.packages.test;
+        build = pkgs.writeShellApplication {
+          name = "build";
+          runtimeInputs = with pkgs; [ docker ];
+          text = ''
+            docker build ${./terraria} -t test-terraria \
+              --build-arg TVERSION=${tVersion} \
+              --build-arg TVERSIONTRIM=${tVersionTrim}
+
+            docker build ${./tmodloader} -t test-tmodloader \
+              --build-arg TMLVERSION=${tmlVersion}
+          '';
+          meta.mainProgram = "build";
+        };
+        test = pkgs.writeShellApplication {
+          name = "test-container";
+          runtimeInputs = with pkgs; [ docker ];
+          text = ''
+            ${build}
+            docker compose up
+          '';
+        };
+        trivy = pkgs.writeShellApplication {
+          name = "trivy-image";
+          runtimeInputs = with pkgs; [ docker trivy ];
+          # Need to supply the image name manually
+          text = ''
+            ${build}
+            trivy image "$1"
+          '';
+        };
       };
     };
   };
